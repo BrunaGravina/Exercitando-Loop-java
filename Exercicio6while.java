fa3f2a5570e57d229bbdbe8a255087b15fec143b
BrunaG <124813648+BrunaGravina@users.noreply.github.com>
2023-04-17 21:37:33 -0300

    Add files via upload

---
+++ Exercicio6while.java
@@ -0,0 +1,14 @@
+package com.mycompany.exercicio6while;
+
+public class Exercicio6while {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        while (i <= 1000) {
+            System.out.println(i);
+            i = i+6;
+        }
+    }
+}
