bbcc9f44e969ad0f667b5142975718f760cffce9
BrunaG <124813648+BrunaGravina@users.noreply.github.com>
2023-04-18 17:07:16 -0300

    Add files via upload

---
+++ Exercicio4dio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio4dio;
+
+/**
+ *
+ * @author bruna
+ */
+public class Exercicio4dio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
