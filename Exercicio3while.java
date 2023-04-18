53895c4c8b1789c4087107c2dc31c4d5e18d52e0
BrunaG <124813648+BrunaGravina@users.noreply.github.com>
2023-04-17 21:20:59 -0300

    Add files via upload

---
+++ Exercicio3while.java
@@ -0,0 +1,18 @@
+
+
+package com.mycompany.exercicio3while;
+
+
+public class Exercicio3while {
+
+    public static void main(String[] args) {
+       
+        int i = 0;
+        
+        while (i <= 1000){
+            if (i % 2 == 0)
+            System.out.println(i);
+            i++;
+        }
+    }
+}
