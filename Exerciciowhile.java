d0f332963c5d2981b5e617efe7a7c353e126819a
BrunaG <124813648+BrunaGravina@users.noreply.github.com>
2023-04-17 21:08:59 -0300

    Add files via upload

---
+++ Exerciciowhile.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exerciciowhile;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bruna
+ */
+public class Exerciciowhile {
+
+    public static void main(String[] args) {
+       Scanner e = new Scanner (System.in);
+       String frase = null;
+       while (!"PARE".equals(frase)){
+           System.out.println("Digite uma frase: ");
+           frase = e.nextLine();
+       }
+       
+    }
+}
