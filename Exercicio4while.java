a2c92d06bd39c5112e326965e952dd1d92ce6da6
BrunaG <124813648+BrunaGravina@users.noreply.github.com>
2023-04-17 21:23:20 -0300

    Add files via upload

---
+++ Exercicio4while.java
@@ -0,0 +1,16 @@
+package com.mycompany.exercicio4while;
+
+public class Exercicio4while {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        while (i <= 1000) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
