bd310639efdc3524a368315a5c5022797e827344
BrunaG <124813648+BrunaGravina@users.noreply.github.com>
2023-04-25 21:12:01 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/* Escreva um programa que leia 10 numeros que o usuario
+possa digitar e imprima a media dos numeros. utilizando um
+loop for;
+*/
+public class Main {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+        int cont = 0;
+        int soma = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Digite um numero: ");
+            int num = e.nextInt();
+            cont++;
+            soma += num;
+        }
+        double media = soma / cont;
+        System.out.println("A media é: " + media);
+
+    }
+}
